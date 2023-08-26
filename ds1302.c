2e0b8ec8abab5bf48c633536f74f060053bd9a9c
Aaron Escoboza <aaron.villegas@hotmail.com>
2023-08-26 07:59:34 -0700

    refactoring and add functions for driving lines individually

--- ds1302.c
+++ ds1302.c
@@ -11,11 +11,12 @@
 /* Definition of sizes */
 #define DS1302_DATA_SIZE           (8)
 #define DS1302_ADDR_SIZE           (8)
-#define DS1302_PACKAGE_SIZE        (DS1302_DATA_SIZE + DS1302_ADDR_SIZE)
+#define DS1302_CMD_SIZE            (8)
+#define DS1302_FRAME_SIZE          (DS1302_DATA_SIZE + DS1302_ADDR_SIZE)
 #define DS1302_RAM_ADDR_START      (0xC0)
 #define DS1302_RAM_SIZE            (31)
 
-/* Register definition according to the spec */
+/* Register definition according to the spajjajaec */
 #define DS1302_REG_SEC              0x80
 #define DS1302_REG_MIN              0x82
 #define DS1302_REG_HOUR             0x84
@@ -28,13 +29,49 @@
 #define BCD_TO_DEC(val)            ((val/16*10) + (val%16))
 #define DEC_TO_BCD(val)            ((val/10*16) + (val%10))
 
+static void set_rst(void)
+{
+    HAL_GPIO_WritePin(DS1302_GPIO_PORT, DS1302_PIN_RST, GPIO_PIN_SET);
+}
+
+static void reset_rst(void)
+{
+    HAL_GPIO_WritePin(DS1302_GPIO_PORT, DS1302_PIN_RST, GPIO_PIN_RESET);
+}
+ 
+static void set_clk(void)
+{
+    HAL_GPIO_WritePin(DS1302_GPIO_PORT, DS1302_PIN_SCLK, GPIO_PIN_SET);
+}
+
+static void reset_clk(void)
+{
+    HAL_GPIO_WritePin(DS1302_GPIO_PORT, DS1302_PIN_SCLK, GPIO_PIN_RESET);
+}
+
+static void set_data(void)
+{
+    HAL_GPIO_WritePin(DS1302_GPIO_PORT, DS1302_PIN_SDA, GPIO_PIN_SET);
+}
+
+static void reset_data(void)
+{
+    HAL_GPIO_WritePin(DS1302_GPIO_PORT, DS1302_PIN_SDA, GPIO_PIN_RESET);
+}
+
+static void set_idle_state(void)
+{
+    HAL_GPIO_WritePin(DS1302_GPIO_PORT, DS1302_PIN_RST, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(DS1302_GPIO_PORT, DS1302_PIN_SDA, GPIO_PIN_RESET);
+}
+
 /**
  * @brief Initialize the DS1302 device
  *
  * @param void
  * @return void
  */
-void DS1302_init(void)
+void ds1302_init(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
 
@@ -46,19 +83,44 @@ void DS1302_init(void)
     HAL_GPIO_WritePin(DS1302_GPIO_PORT, DS1302_PIN_SCLK, GPIO_PIN_RESET);
 }
 
+static void set_read_mode(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.Pin = DS1302_PIN_SDA;
+    GPIO_InitStructure.Mode =  GPIO_MODE_INPUT;
+    GPIO_InitStructure.Pull = GPIO_PULLDOWN;
+    GPIO_InitStructure.Speed = GPIO_SPEED_HIGH;
+    HAL_GPIO_Init(DS1302_GPIO_PORT, &GPIO_InitStructure);
+}
+
+static void set_write_mode(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.Pin = DS1302_PIN_SDA;
+    GPIO_InitStructure.Mode =  GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStructure.Speed = GPIO_SPEED_HIGH;
+    HAL_GPIO_Init(DS1302_GPIO_PORT, &GPIO_InitStructure);
+}
+
+
 /**
  * @brief write a bit following serial communication specified in the spec
  *
  * @param bit you being written
  * @return void
  */
-static void DS1302_writeBit(uint8_t bit)
+static void write_bit(uint8_t bit)
 {
-    HAL_GPIO_WritePin(DS1302_GPIO_PORT, DS1302_PIN_SDA, (bit == 1) ?  GPIO_PIN_SET :  GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(DS1302_GPIO_PORT, DS1302_PIN_SCLK,  GPIO_PIN_SET);
-    HAL_Delay(1);
-    HAL_GPIO_WritePin(DS1302_GPIO_PORT, DS1302_PIN_SCLK,  GPIO_PIN_RESET);
-    HAL_Delay(1);
+    if (bit)
+    {
+        set_data();
+    }
+    else
+    {
+        reset_data();
+    }
+    set_clk();
+    reset_clk();
 }
 
 /**
@@ -68,24 +130,50 @@ static void DS1302_writeBit(uint8_t bit)
  * @param data that will write to addr
  * @return void
  */
-static void DS1302_writeByte(uint8_t addr, uint8_t data)
+static void write_byte(uint8_t addr, uint8_t data)
 {
     uint8_t i;
-    uint16_t  package = (uint16_t)data << 8 | addr;
+    uint16_t  frame = (uint16_t)data << 8 | addr;
 
     /* RST high to start communication */
-    HAL_GPIO_WritePin(DS1302_GPIO_PORT, DS1302_PIN_RST, GPIO_PIN_SET);
+    set_rst();
 
-    /* write address/cmd and then the data */
-    for (i = 0; i < DS1302_PACKAGE_SIZE; i ++)
+    /* write address and then the data */
+    for (i = 0; i < DS1302_FRAME_SIZE; i ++)
     {
-        DS1302_writeBit(package & 1);
-        package >>= 1;
+        write_bit(frame & 1);
+        frame >>= 1;
     }
 
-    /* Go back to default state*/
-    HAL_GPIO_WritePin(DS1302_GPIO_PORT, DS1302_PIN_RST, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(DS1302_GPIO_PORT, DS1302_PIN_SDA, GPIO_PIN_RESET);
+    set_idle_state();
+}
+
+/**
+ * @brief write a command
+ *
+ * @param cmd to write
+ * @return void
+ */
+static void write_cmd(uint8_t cmd)
+{
+	uint8_t i;
+    for (i = 0; i < DS1302_CMD_SIZE; i ++)
+    {
+        write_bit(cmd & 1);
+        cmd >>= 1;
+    }
+}
+
+uint8_t read_data_pin(void)
+{
+    if (HAL_GPIO_ReadPin(DS1302_GPIO_PORT, DS1302_PIN_SDA) == GPIO_PIN_SET)
+    {
+        return 1;
+    }
+    else 
+    {
+        return 0; 
+    }
 }
 
 /**
@@ -94,36 +182,31 @@ static void DS1302_writeByte(uint8_t addr, uint8_t data)
  * @param addr address you want to read from
  * @return byte read
  */
-static uint8_t DS1302_readByte(uint8_t addr)
+static uint8_t read_byte(uint8_t addr)
 {
     uint8_t i;
     uint8_t data = 0;
 
     /* RST high to start communication */
-    HAL_GPIO_WritePin(DS1302_GPIO, DS1302_PIN_RST, GPIO_PIN_SET);
+    reset_rst();
 
     /* Make sure LSB bit is high for read */
     addr |= 0x1;
-    DS1302_writeByte(addr);
+    write_cmd(addr);
 
-    DS1302_setSDAasInput();
+    set_read_mode();
     for (i = 0; i < DS1302_DATA_SIZE; i ++)
     {
-        if (HAL_GPIO_ReadPin(DS1302_GPIO, DS1302_PIN_SDA))
+        if (read_data_pin)
         {
             data |= 0x80;
         }
-        HAL_GPIO_WritePin(DS1302_GPIO, DS1302_PIN_SCLK,  GPIO_PIN_SET);
-        Hal_delay(1);
-        HAL_GPIO_WritePin(DS1302_GPIO, DS1302_PIN_SCLK,  GPIO_PIN_RESET);
-        Hal_delay(1);
+        set_clk();
+        reset_clk();
         data >>= 1;
     }
-    DS1302_setSDAasOutput();
-
-    /* Go back to default state*/
-    HAL_GPIO_WritePin(DS1302_GPIO, DS1302_PIN_RST, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(DS1302_GPIO, DS1302_PIN_SDA, GPIO_PIN_RESET);
+    set_write_mode();
+    set_idle_state();
 
     return data;
 }
@@ -134,18 +217,18 @@ static uint8_t DS1302_readByte(uint8_t addr)
  * @param *time pointer to Time structure
  * @return void
  */
-void DS1302_getTime(Time *time)
+void ds1302_get_time(Time *time)
 {
     if (time == NULL)
         return;
 
-    time->year = BCD_TO_DEC(DS1302_readByte(DS1302_REG_YEAR));
-    time->month = BCD_TO_DEC(DS1302_readByte(DS1302_REG_MONTH));
-    time->date = BCD_TO_DEC(DS1302_readByte(DS1302_REG_DATE));
-    time->hour = BCD_TO_DEC(DS1302_readByte(DS1302_REG_HOUR));
-    time->min = BCD_TO_DEC(DS1302_readByte(DS1302_REG_MIN));
-    time->sec = BCD_TO_DEC(DS1302_readByte(DS1302_REG_SEC));
-    time->day = BCD_TO_DEC(DS1302_readByte(DS1302_REG_DAY));
+    time->year = BCD_TO_DEC(read_byte(DS1302_REG_YEAR));
+    time->month = BCD_TO_DEC(read_byte(DS1302_REG_MONTH));
+    time->date = BCD_TO_DEC(read_byte(DS1302_REG_DATE));
+    time->hour = BCD_TO_DEC(read_byte(DS1302_REG_HOUR));
+    time->min = BCD_TO_DEC(read_byte(DS1302_REG_MIN));
+    time->sec = BCD_TO_DEC(read_byte(DS1302_REG_SEC));
+    time->day = BCD_TO_DEC(read_byte(DS1302_REG_DAY));
 }
 
 /**
@@ -154,27 +237,25 @@ void DS1302_getTime(Time *time)
  * @param *time pointer to Time structure
  * @return void
  */
-void DS1302_setTime(Time *time)
+void ds1302_setTime(Time *time)
 {
 
     if (time == NULL)
         return;
 
     /* Enable write by driving protected bit to 0*/
-    DS1302_writeByte(DS1302_REG_CONTROL, 0);
-    HAL_delay(1);
+    write_byte(DS1302_REG_CONTROL, 0);
 
     /* Write time to into registers in BCD format*/
-    DS1302_writeByte(DS1302_REG_YEAR, DEC_TO_BCD(time->year));
-    DS1302_writeByte(DS1302_REG_MONTH, DEC_TO_BCD(time->month));
-    DS1302_writeByte(DS1302_REG_DATE, DEC_TO_BCD(time->date));
-    DS1302_writeByte(DS1302_REG_HOUR, DEC_TO_BCD(time->hour));
-    DS1302_writeByte(DS1302_REG_MIN, DEC_TO_BCD(time->min));
-    DS1302_writeByte(DS1302_REG_SEC, DEC_TO_BCD(time->sec));
+    write_byte(DS1302_REG_YEAR, DEC_TO_BCD(time->year));
+    write_byte(DS1302_REG_MONTH, DEC_TO_BCD(time->month));
+    write_byte(DS1302_REG_DATE, DEC_TO_BCD(time->date));
+    write_byte(DS1302_REG_HOUR, DEC_TO_BCD(time->hour));
+    write_byte(DS1302_REG_MIN, DEC_TO_BCD(time->min));
+    write_byte(DS1302_REG_SEC, DEC_TO_BCD(time->sec));
 
     /* Disable write by driving protected bit to 1 */
-    DS1302_writeByte(DS1302_REG_CONTROL,0x80);
-    HAL_delay(1);
+    write_byte(DS1302_REG_CONTROL,0x80);
 }
 
 /**
@@ -183,20 +264,18 @@ void DS1302_setTime(Time *time)
  * @param data data to write into the address
  * @return void
  */
-void DS1302_writeToRAM(uint8_t addr, uint8_t data)
+void ds1302_write_ram(uint8_t addr, uint8_t data)
 {
     /* Check for valid addr */
     if ( (addr >= 0) && (addr <= 30) )
         return;
 
     /* Enable write by driving protected bit to 0*/
-    DS1302_writeByte(DS1302_REG_CONTROL, 0);
-    HAL_delay(1);
+    write_byte(DS1302_REG_CONTROL, 0);
     /* Write addresses for RAM are multiple of 2 */
-    DS1302_writeByte(DS1302_RAM_ADDR_START + (2 * addr), data);
+    write_byte(DS1302_RAM_ADDR_START + (2 * addr), data);
     /* Disable write by driving protected bit to 1 */
-    DS1302_WriteByte(DS1302_REG_CONTROL,0x80);
-    delayUS_DWT(1);
+    write_byte(DS1302_REG_CONTROL,0x80);
 }
 
 /**
@@ -204,13 +283,13 @@ void DS1302_writeToRAM(uint8_t addr, uint8_t data)
  * @param addr address you want to read from
  * @return data read
  */
-uint8_t DS1302_readFromRAM(uint8_t addr)
+uint8_t ds1302_read_ram(uint8_t addr)
 {
     /* Check for valid addr */
     if ( (addr >= 0) && (addr <= 30) )
-        return;
+        return 0;
 
-    return DS1302_readByte(DS1302_RAM_ADDR_START + (2 * addr));
+    return read_byte(DS1302_RAM_ADDR_START + (2 * addr));
 }
 
 /**
@@ -218,11 +297,11 @@ uint8_t DS1302_readFromRAM(uint8_t addr)
  * @param void
  * @return void
  */
-void DS1302_clearRAM(void)
+void ds1302_clear_ram(void)
 {
     int i;
     for (i = 0; i < DS1302_RAM_SIZE; i++)
     {
-        DS1302_writeToRAM(i, 0);
+        ds1302_write_ram(i, 0);
     }
 }
--- ds1302.h
+++ ds1302.h
@@ -10,13 +10,14 @@
 #define __DS1302__H
 
 #include "stm32f4xx.h"
+#include "stdint.h"
 
-/************* Configuration section /**************/
-#define DS1302_GPIO_PORT            GPIOA
-#define DS1302_PIN_SCLK             GPIO_PIN_NUM_1
-#define DS1302_PIN_SDA              GPIO_PIN_NUM_2
-#define DS1302_PIN_RST              GPIO_PIN_NUM_3
-/************* Configuration section /**************/
+/************* Configuration section **************/
+#define DS1302_GPIO_PORT            GPIOB
+#define DS1302_PIN_SCLK             GPIO_PIN_7
+#define DS1302_PIN_SDA              GPIO_PIN_6
+#define DS1302_PIN_RST              GPIO_PIN_5
+/************* Configuration section **************/
 
 /**
  * Enumeration for days of the week
@@ -46,17 +47,16 @@ typedef struct
     uint8_t sec;
 }Time;
 
-
 /* Initialize DS1302 device */
-void DS1302_init(void);
+void ds1302_init(void);
 
 /* APIs for calendar*/
-void DS1302_getTime(Time *time);
-void DS1302_setTime(Time *time);
+void ds1302_get_time(Time *time);
+void ds1302_setTime(Time *time);
 
 /* APIs for RAM*/
-void DS1302_clearRAM(void);
-void DS1302_writeToRAM(uint8_t addr, uint8_t data);
-uint8_t DS1302_readFromRAM(uint8_t addr);
+void ds1302_clear_ram(void);
+void ds1302_write_ram(uint8_t addr, uint8_t data);
+uint8_t ds1302_read_ram(uint8_t addr);
 
 #endif
